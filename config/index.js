275922a123e86e3249f1a3813485c7684882d017
Anton Mokrushin <anton@mokr.org>
2018-03-23 12:23:50 +0300

    set assets public patch

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
         // Paths
         assetsRoot: path.resolve(__dirname, '../dist'),
         assetsSubDirectory: 'static',
-        assetsPublicPath: '/',
+        assetsPublicPath: '/dummy-spa-calc-fractions-and-websocket/',
 
         /**
          * Source Maps
